ea7aa89a0bfdfc055bc5b9ba5b61aa78311bdb8c
XBigTK13X <xbigtk13x@gmail.com>
2016-12-12 16:51:36 -0600

    Handle cases where a game has no listed image

--- source/game.py
+++ source/game.py
@@ -4,18 +4,21 @@ import giantbomb
 
 class Game():
 
-    def __init__(self, rawData):
-        self.Title = rawData['game']['name']
-        self.Platforms = [rawData['platform']['name']]
-        self.PrettyReleaseDate = rawData['olavaSearchDate']
-        self.ReleaseDate = datetime.strptime(self.PrettyReleaseDate, '%Y-%m-%d')
-        self.Slug = self.Title + '-' + self.PrettyReleaseDate
-        self.ReleaseDay = self.ReleaseDate.strftime('%A')
-        self.DetailLink = rawData['site_detail_url'].split('/releases/')[0]
-        self.CalendarTitle = self.Title.replace(' ', '+')
-        self.CalendarDate = self.PrettyReleaseDate.replace('-', '')
-        self.Info = giantbomb.game_info(rawData['game']['api_detail_url'])
-        if 'results' in self.Info:
-            self.Info = self.Info['results']
-            self.Description = self.Info['deck']
-            self.Image = self.Info['image']['small_url']
+	def __init__(self, rawData):
+		self.Title = rawData['game']['name']
+		self.Platforms = [rawData['platform']['name']]
+		self.PrettyReleaseDate = rawData['olavaSearchDate']
+		self.ReleaseDate = datetime.strptime(self.PrettyReleaseDate, '%Y-%m-%d')
+		self.Slug = self.Title + '-' + self.PrettyReleaseDate
+		self.ReleaseDay = self.ReleaseDate.strftime('%A')
+		self.DetailLink = rawData['site_detail_url'].split('/releases/')[0]
+		self.CalendarTitle = self.Title.replace(' ', '+')
+		self.CalendarDate = self.PrettyReleaseDate.replace('-', '')
+		self.Info = giantbomb.game_info(rawData['game']['api_detail_url'])
+		if 'results' in self.Info:
+			if 'results' in self.Info:
+				self.Info = self.Info['results']
+				if 'deck' in self.Info:
+					self.Description = self.Info['deck']
+				if 'image' in self.Info and self.Info['image'] != None and 'small_url' in self.Info['image']:
+					self.Image = self.Info['image']['small_url']
